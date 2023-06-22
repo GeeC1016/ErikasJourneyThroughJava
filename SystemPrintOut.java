a704e9379c7002c8feb88a282e9f414305fd020f
Erika Quamina <erikaquamina@hotmail.com>
2023-06-22 18:05:34 -0400

    I'm just writing some lines of code to the screen. Woohoogit status

---
+++ SystemPrintOut.java
@@ -0,0 +1,9 @@
+class SystemPrintOut {
+	public static void main (String [] args){
+		System.out.println("I'm printing random text to the screen");
+		System.out.println("I'm printing random text to the screen");
+		System.out.println("I'm printing random text to the screen");
+		System.out.println("It's just my first lesson, so not super impressive... I know");
+		System.out.println("But it can only get better from here... right?");
+	}
+}
